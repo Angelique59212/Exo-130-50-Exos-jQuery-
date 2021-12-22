98b0cfed0d4b952dfefc7114883031f70233879f
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-22 16:30:35 +0100

    =>8

--- js/app.js
+++ js/app.js
@@ -24,3 +24,20 @@ $("tr:first-child").siblings(':even').css('backgroundColor' , 'pink');
 $("#print").click(function () {
         window.print();
 })
+
+//limit the number of characters textarea
+function limitText(item, maxCharacters) {
+    item.attr("maxCharacters", maxCharacters.toString())
+}
+limitText($("#textarea"), 100);
+
+//first word of a paragraph
+// let p = $('.para');
+// let word = [];
+// word = p.text().split(' ', 1);
+// console.log(word);
+// p.text(word).css("fontWeight", "bold")
+
+//create div
+let result = $("#result");
+result.html(result.html() + "<div>Ajout d'un div</div>")
